2955dbd3338a4daa59375ad0638f374c437d62ea
Raghavendra Tiwari <119629926+raghavendratiwari@users.noreply.github.com>
2024-04-18 10:27:57 +0530

    updated port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('FILE MANAGEMENT SYSTEM ON AWS');
   res.end();
-}).listen(3000);
+}).listen(80);
 
 
 const aws = require('aws-sdk');
